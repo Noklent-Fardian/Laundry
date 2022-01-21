41c5e8a9288a777726594fc9c4df905da6f267a6
Noklent Fardian <noklent320@gmail.com>
2022-01-21 08:33:31 +0700

    add service price

--- SMK-Laundry/Header Transaction.vb
+++ SMK-Laundry/Header Transaction.vb
@@ -4,9 +4,7 @@ Public Class Header_Transaction
     Dim conn As SqlConnection
     Dim cmd As SqlCommand
     Dim dr As SqlDataReader
-    Dim query, id_package, id_customer As String
-
-
+    Dim query, id_service, id_customer, id_transaction As String
     Sub koneksi()
         conn = New SqlConnection("Server=NOX; Database=Laundry; Integrated Security=True")
         If conn.State = ConnectionState.Closed Then
@@ -14,28 +12,52 @@ Public Class Header_Transaction
         End If
     End Sub
 
-    Private Sub total_box_TextChanged(sender As Object, e As EventArgs) Handles service_box.TextChanged
+    Private Sub total_box_keypress(sender As Object, e As KeyPressEventArgs) Handles total_box.KeyPress
+        Dim kunci As Short = Asc(e.KeyChar)
+        If (e.KeyChar Like "[0-9]" _
+            OrElse kunci = Keys.Back) Then
+            kunci = 0
+        End If
+        e.Handled = CBool(kunci)
+
+    End Sub
+    Sub service()
+        query = "select * from service order by name_service"
+        ComboBox1.DataSource = read(query)
+        ComboBox1.ValueMember = "name_service"
+        ComboBox1.DisplayMember = "name_service"
 
     End Sub
 
-    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+    Private Sub Header_Transaction_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call service()
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Call koneksi()
+        query = "select * from service where name_service='" & ComboBox1.Text & "'"
+        cmd = New SqlCommand(query, conn)
+        dr = cmd.ExecuteReader
+        dr.Read()
+        If dr.HasRows Then
+            id_service = dr.Item("id")
+            price_box.Text = dr.Item("price_unit_service")
+
+        End If
 
     End Sub
 
-    Sub add_btn()
-        Dim edit_btn, delete_btn As New DataGridViewButtonColumn
-        edit_btn.HeaderText = "Edit"
-        edit_btn.Text = "Edit"
-        edit_btn.Name = "Edit"
-        edit_btn.FlatStyle = FlatStyle.Flat
-        edit_btn.UseColumnTextForButtonValue = True
-        datagrid_view.Columns.Add(edit_btn)
-        delete_btn.HeaderText = "Delete"
-        delete_btn.Text = "Delete"
-        delete_btn.Name = "Delete"
-        delete_btn.FlatStyle = FlatStyle.Flat
-        delete_btn.UseColumnTextForButtonValue = True
-        datagrid_view.Columns.Add(edit_btn)
+    Sub kosong()
+        Phone_box.Text = ""
+        price_box.Text = ""
+        total_box.Text = ""
+        addres_label.Text = ""
+        name_label.Text = ""
+    End Sub
+
+    Sub kondisi_awal()
+        query = ""
+
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MainMenu.Show()
